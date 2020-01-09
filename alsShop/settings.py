65a2a26f192c500cc1559ef28a74ad5094c8b190
MAC_DEV <mcamacho1990@icloud.com>
2020-01-08 19:28:47 -0500

    DEBUGfalseProduction

--- alsShop/settings.py
+++ alsShop/settings.py
@@ -15,7 +15,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False 
 
 ALLOWED_HOSTS = [
     'als-bikestore.herokuapp.com','127.0.0.1',
